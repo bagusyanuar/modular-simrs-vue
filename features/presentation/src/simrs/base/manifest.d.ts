467b11b569fa7d31829fe6716e4fe9bb31cd6525
bagusyanuar <bagus.yanuar613@gmail.com>
2026-04-13 15:21:07 +0700

    clean up build result

--- features/presentation/src/simrs/base/manifest.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Master Manifest - Base Product
- * Mendaftarkan modul-modul yang aktif secara default di SIMRS.
- */
-export declare const activeModules: string[];
-//# sourceMappingURL=manifest.d.ts.map
