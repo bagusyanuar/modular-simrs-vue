15b2ce33b765f9db258d100c55b11c2d020db9a7
bagusyanuar <bagus.yanuar613@gmail.com>
2026-04-16 16:25:51 +0700

    fix(old-app): cleanup auth

--- apps/old-app/src/core/repositories/auth.repository.ts
+++
@@ -1,6 +0,0 @@
-import type { LoginForm } from '@/core/domains/inputs';
-import type { Auth } from '@/core/domains/models';
-
-export interface AuthRepository {
-  login(input: LoginForm): Promise<Auth>;
-}
